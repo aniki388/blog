f6df631ae25839f74112e6fa42d5cd2b3110e14c
aniki388 <3069859233@qq.com>
2024-08-23 18:16:42 +0800

    Site updated: 2024-08-23 18:16:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/4a17b156.html","posts/bdae0d9b.html"];function toRandomPost(){
+var posts=["posts/44ed1e9b.html","posts/4a17b156.html","posts/bdae0d9b.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
