61c0fb8e9ff7b60df9b8bedc8072fc9c28c886a0
aniki388 <3069859233@qq.com>
2024-08-25 11:09:25 +0800

    Site updated: 2024-08-25 11:09:23

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/44ed1e9b.html","posts/4a17b156.html","posts/84f3211d.html","posts/bdae0d9b.html"];function toRandomPost(){
+var posts=["posts/44ed1e9b.html","posts/84f3211d.html","posts/bdae0d9b.html","posts/4a17b156.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
